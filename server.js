6905c179a779c0dc7dcae312ae146272c474e76d
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2018-05-20 12:29:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname));
 app.get('/', function(req,res) {
 //   res.type('text/plain');
