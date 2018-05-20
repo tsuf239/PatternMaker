eb0f8a3c2b15595ff6f33c8dfe421b7288847ab3
tsuf239 <tsuftsuf98@gmail.com>
2018-05-20 12:09:52 +0300

    Add All Files

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname));
+app.get('/', function(req,res) {
+//   res.type('text/plain');
+  res.sendFile('pictureMaker.html', {root: __dirname});
+});
+
+app.listen(app.get('port'), ()=> {
+  console.log('up and runing on port '+ app.get('port'));
+})
